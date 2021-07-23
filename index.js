09d0126aa5d1b81e1d6d91ceb59b1dc3c6907117
Dasha <dasha.kotova.mail@gmail.com>
2021-07-23 16:20:07 -0400

    add intervals

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
   let currentIndex = 0 // so first div in our grid
   let appleIndex = 0 //so first div in our grid
-  let currentSnake = [2,1,0]
+  let currentSnake = [2,1,0] //the head is 2; the tail is 0
+
+  let direction = 1
+  let score = 0
+  let speed = 0.9
+  let intervalTime = 0
+  let interval = 0
 
 })
