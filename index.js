2866297a3f1a0216928a26fc20564143793c7a3b
Dasha <dasha.kotova.mail@gmail.com>
2021-07-26 19:20:31 -0400

    modify snake collision function

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
       (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
       (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
       (currentSnake[0] - width < 0 && direction === -width) //if snake hits the top
-      squares[currentSnake]
-    )
+      squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
+    ) {
+      return clearInterval(interval) //clear the interval if any of the above happens
+    }
 
   }
 
